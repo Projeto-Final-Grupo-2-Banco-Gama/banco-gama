ec5a4edebc4339357c760e0543273610da452cc5
FabricioMessias <fabriciomessias@live.com>
2021-10-14 20:58:54 -0300

    criação de atributos

--- projetofinal/src/main/java/br/itau/projetofinal/model/Cliente.java
+++ projetofinal/src/main/java/br/itau/projetofinal/model/Cliente.java
@@ -1,5 +1,9 @@
 package br.itau.projetofinal.model;
 
 public class Cliente {
-    
+    private long codigo;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
 }
--- projetofinal/src/main/java/br/itau/projetofinal/model/Conta.java
+++ projetofinal/src/main/java/br/itau/projetofinal/model/Conta.java
@@ -1,5 +1,9 @@
 package br.itau.projetofinal.model;
 
 public class Conta {
-    
+    private long numero;
+    private long agencia;
+    private long tipo;
+    private double saldo;
+    private Cliente titular;
 }
