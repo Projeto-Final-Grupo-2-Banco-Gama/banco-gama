8285ef170e8a83095964a30a4cf342efd9f79b47
syaoranea <eduardo_rocha87@hotmail.com>
2021-10-15 15:11:39 -0300

    model atualização

--- projetofinal/src/main/java/br/itau/projetofinal/model/Cliente.java
+++ projetofinal/src/main/java/br/itau/projetofinal/model/Cliente.java
@@ -1,9 +1,75 @@
 package br.itau.projetofinal.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
     private long codigo;
+
+    @Column(name = "nome", length = 200, nullable = false) 
     private String nome;
+
+    @Column(name = "cpf", length = 200, nullable = false, unique = true) 
     private String cpf;
+
+    @Column(name = "telefone", length = 200)
     private String telefone;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
     private String email;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
 }
