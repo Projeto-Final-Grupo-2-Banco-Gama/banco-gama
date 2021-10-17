86b4a1787a79d341dc46d2dec3d44490da5ea505
Fernando Kendi <fedked@outlook.com>
2021-10-17 16:32:01 -0300

    criado na consulta listar todos clientes

--- projetofinal/src/main/java/br/itau/projetofinal/controller/ClienteController.java
+++ projetofinal/src/main/java/br/itau/projetofinal/controller/ClienteController.java
@@ -1,5 +1,28 @@
 package br.itau.projetofinal.controller;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.itau.projetofinal.model.Cliente;
+import br.itau.projetofinal.repository.ClienteRepo;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/cliente")
 public class ClienteController {
-    
+
+    @Autowired
+    private ClienteRepo repo;
+
+    @GetMapping("/todos")
+    public List<Cliente> listarTodos() {
+        List<Cliente> lista = (List<Cliente>) repo.findAll();
+        return lista;
+    }
 }
