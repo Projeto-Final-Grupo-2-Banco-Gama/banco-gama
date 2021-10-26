de13bcff51a7082270d624897ace0ffd5524d81b
FabricioMessias <fabriciomessias@live.com>
2021-10-26 14:20:24 -0300

    novas consultas adicionadas

--- projetofinal/src/main/java/br/itau/projetofinal/repository/ClienteRepo.java
+++ projetofinal/src/main/java/br/itau/projetofinal/repository/ClienteRepo.java
@@ -2,6 +2,8 @@ package br.itau.projetofinal.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import br.itau.projetofinal.model.Cliente;
@@ -9,4 +11,13 @@ import br.itau.projetofinal.model.Cliente;
 public interface ClienteRepo extends CrudRepository<Cliente, Long>{
     
     public List<Cliente> findAll();
+
+    public Cliente findByNome(String nome);
+
+    public List<Cliente> findAllByNome(String nome);
+
+    public List<Cliente> findAllByOrderByNome();
+
+    // utiliza o conceito de páginas (paginação) para retornar as informações
+    public Page<Cliente> findAll(Pageable pageable);
 }
